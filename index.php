de70a223d179c0695d49390231c3ec31f0a8c8fe
Ilya12346 <160493090+Ilya12346@users.noreply.github.com>
2024-02-22 12:53:28 +0300

    cr

--- index.php
+++ index.php
@@ -4,34 +4,14 @@ $APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");
 ?>
 Пустая страница. <a href="/bitrix/admin/">Перейти в Панель &nbsp;Управления</a>
 <?php
-// 	$APPLICATION->IncludeComponent(
-// 	"bitrix:main.include",
-// 	"template2", 
-// 	[
-// 		"AREA_FILE_SHOW" => "file",	// Показывать включаемую область
-// 		"PATH" => SITE_TEMPLATE_PATH . "/components/bitrix/main.include/template2/template.php",	// Путь к файлу области
-// 	],
-// 	false
-// );
 
-
-// $APPLICATION->IncludeComponent(
-// 	"bitrix:main.include",
-// 	"",
-// 	Array(
-// 	"AREA_FILE_SHOW" => "file", 
-// 	"PATH" => SITE_TEMPLATE_PATH . "/test.php",	// Путь к файлу области
-// 	)
-// );
-
-	
 
 ?>
 <?php 
 
 	// echo '<pre>';
 	// var_dump(SITE_TEMPLATE_PATH);
-	echo '</pre>';
+	// echo '</pre>';
 
 ?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
